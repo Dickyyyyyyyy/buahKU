acd520c50ce313985c28f1efb4966c8bafc2e001
unknown <marinafebiyol@gmail.com>
2025-11-26 12:28:20 +0700

    membuat website terbaru

--- src/app.js
+++ src/app.js
@@ -14,69 +14,67 @@ document.addEventListener('alpine:init', () => {
         ],
     }));
     
-    Alpine.store("cart", {
-    items: [],
-    quantity: 0,
-    total: 0,
+    Alpine.store('cart',{
+        items: [],
+        total:0,
+        quantity:0,
+        add(newItem){
+            //cek apakah ada barang yang sama di cart
+            const cartItem = this.items.find((item) => item.id === newItem.id);
 
-    add(product) {
-      const found = this.items.find((item) => item.id === product.id);
+            //jika belum ada / cart masih kosong
+            if(!cartItem){
 
-      if (!found) {
-        this.items.push({
-          ...product,
-          quantity: 1,
-          total: product.price,
-        });
-        this.quantity++;
-        this.total += product.price;
-      } else {
-        found.quantity++;
-        found.total = found.quantity * found.price;
-        this.quantity++;
-        this.total += found.price;
-      }
-    },
+                this.items.push({...newItem, quantity: 1, total: newItem.price});
+                this.quantity++;
+                this.total += newItem.price;
+              
+            } else{
+                //jika barang sudah ada, cek apakah barang beda atau sama dengan yang ada di cart
+                this.items = this.items.map((item)=>{
+                    //jika barang berbeda
+                    if (item.id !== newItem.id){
+                        return item;
+                    } else{
+                        //jika barang sudah ada, tambah jumlah quantity dan totalnya
+                        item.quantity++;
+                        item.total = item.price * item.quantity;
+                        this.quantity++;
+                        this.total += item.price;
+                        return item;
+                    }
+                });
+            }
+        },
+        remove(id){
+            //ambil item yang mau di remove berdasarkan id
+            const cartItem = this.items.find((item) => item.id === id);
 
-    remove(id) {
-      const index = this.items.findIndex((item) => item.id === id);
-      if (index === -1) return;
+            //jika item lebih dari 1
+            if(cartItem.quantity > 1) {
+                //telusuri 1 1 
+                this.items =  this.items.map((item) => {
+                    //jika bukan barang yang diklik
+                    if (item.id != id) {
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                })
+            } else if (cartItem.quantity === 1) {
+                //jika barangnya sisa 1
+                this.items = this.items.filter((item) => item.id !== id);
+                this.quantity--;
+                this.total -= cartItem.price;
+            }
 
-      const item = this.items[index];
-      this.quantity--;
-      this.total -= item.price;
+        }
+    });
 
-      item.quantity--;
-      item.total = item.quantity * item.price;
-
-      if (item.quantity === 0) {
-        this.items.splice(index, 1);
-      }
-    },
-  });
-
-  Alpine.data("products", () => ({
-    items: [
-      {
-        id: 1,
-        name: "Apel Merah",
-        price: 15000,
-        img: "apel.jpg",
-      },
-      {
-        id: 2,
-        name: "Jeruk Manis",
-        price: 12000,
-        img: "jeruk.jpg",
-      },
-      {
-        id: 3,
-        name: "Pisang Cavendish",
-        price: 10000,
-        img: "pisang.jpg",
-      },
-    ],
-  }));
 });
 
 //konversi ke rupiah
