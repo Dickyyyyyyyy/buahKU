1823b1e65c62d97db724fb3673e0ac2281840f95
unknown <marinafebiyol@gmail.com>
2025-11-28 15:44:47 +0700

    membuat payment gateaway timbangan

--- src/app.js
+++ src/app.js
@@ -39,16 +39,16 @@ document.addEventListener('alpine:init', () => {
 
     Alpine.data('products', () => ({
         items: [
-            {id:1, name:'Apel', price:20000, img:'apel.jpg'},
-            {id:2, name:'Pisang', price:18000, img:'pisang.jpg'},
-            {id:3, name: 'Anggur', img: 'anggur.jpg', price: 25000},
-            {id:4, name: 'Jeruk', img: 'jeruk.jpg', price: 27000},
-            {id:5, name: 'Kiwi', img: 'kiwi.jpg', price: 32000},
-            {id:6, name: 'Semangka', img: 'semangka.jpg', price: 21000},
-            {id:7, name: 'Mangga', img: 'mangga.jpg', price: 22000},
-            {id:8, name: 'Pepaya', img: 'pepaya.jpg', price: 14000},
-            {id:9, name: 'Stroberi', img: 'stroberi.jpg', price: 39000},
-            {id:10, name: 'Alpukat', img: 'alpukat.jpg', price: 21500},
+            { id: 1, name: 'Apel',      price: 20000, img: 'apel.jpg' },
+            { id: 2, name: 'Pisang',    price: 18000, img: 'pisang.jpg' },
+            { id: 3, name: 'Anggur',    price: 25000, img: 'anggur.jpg' },
+            { id: 4, name: 'Jeruk',     price: 27000, img: 'jeruk.jpg' },
+            { id: 5, name: 'Kiwi',      price: 32000, img: 'kiwi.jpg' },
+            { id: 6, name: 'Semangka',  price: 21000, img: 'semangka.jpg' },
+            { id: 7, name: 'Mangga',    price: 22000, img: 'mangga.jpg' },
+            { id: 8, name: 'Pepaya',    price: 14000, img: 'pepaya.jpg' },
+            { id: 9, name: 'Stroberi',  price: 39000, img: 'stroberi.jpg' },
+            { id:10, name: 'Alpukat',   price: 28000, img: 'alpukat.jpg' }
         ]
     }));
 
