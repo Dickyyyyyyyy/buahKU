e61d74339cc808472501a77e37a7df157f3e106c
unknown <marinafebiyol@gmail.com>
2025-11-28 17:11:27 +0700

    website payment

--- src/app.js
+++ src/app.js
@@ -1,59 +1,62 @@
-document.addEventListener("alpine:init", () => {
-  Alpine.data("products", () => ({
-    items: [
-      { id: 1, name: "Apel Merah", price: 20000, img: "apel.jpg" },
-      { id: 2, name: "Pisang Cavendish", price: 15000, img: "pisang.jpg" },
-      { id: 3, name: "Anggur Merah", price: 25000, img: "anggur.jpg" },
-      { id: 4, name: "Jeruk Manis", price: 18000, img: "jeruk.jpg" },
-      { id: 5, name: "Mangga Harum Manis", price: 30000, img: "mangga.jpg" },
-      { id: 6, name: "Semangka Merah", price: 35000, img: "semangka.jpg" },
-      { id: 7, name: "Nanas Madu", price: 22000, img: "nanas.jpg" },
-      { id: 8, name: "Pepaya California", price: 17000, img: "pepaya.jpg" }
-    ],
-  }));
+document.addEventListener('alpine:init', () => {
+    Alpine.store('cart', {
+        items: [],
 
-  Alpine.store("cart", {
-    items: [],
-    total: 0,
-    quantity: 0,
+        get quantity() {
+            return this.items.reduce((acc, item) => acc + item.quantity, 0);
+        },
 
-    add(product) {
-      let item = this.items.find((i) => i.id === product.id);
+        get total() {
+            return this.items.reduce((acc, item) => acc + item.total, 0);
+        },
 
-      if (!item) {
-        this.items.push({ ...product, quantity: 1, total: product.price });
-      } else {
-        item.quantity++;
-        item.total = item.price * item.quantity;
-      }
+        add(product) {
+            const found = this.items.find(item => item.id === product.id);
 
-      this.updateCart();
-    },
+            if (found) {
+                found.quantity++;
+                found.total = found.quantity * found.price;
+            } else {
+                this.items.push({
+                    ...product,
+                    quantity: 1,
+                    total: product.price
+                });
+            }
+        },
 
-    remove(id) {
-      let item = this.items.find((i) => i.id === id);
+        remove(id) {
+            const index = this.items.findIndex(item => item.id === id);
 
-      if (item.quantity > 1) {
-        item.quantity--;
-        item.total = item.price * item.quantity;
-      } else {
-        this.items = this.items.filter((i) => i.id !== id);
-      }
+            if (this.items[index].quantity > 1) {
+                this.items[index].quantity--;
+                this.items[index].total -= this.items[index].price;
+            } else {
+                this.items.splice(index, 1);
+            }
+        }
+    });
 
-      this.updateCart();
-    },
+    Alpine.data('products', () => ({
+        items: [
+  {id:1, name:'Apel', price:20000, img:'apel.jpg'},
+  {id:2, name:'Pisang', price:18000, img:'pisang.jpg'},
+  {id:3, name:'Anggur', price:25000, img:'apel.jpg'},
+  {id:4, name:'Jeruk', price:27000, img:'apel.jpg'},
+  {id:5, name:'Kiwi', price:32000, img:'apel.jpg'},
+  {id:6, name:'Semangka', price:21000, img:'apel.jpg'},
+  {id:7, name:'Mangga', price:22000, img:'apel.jpg'},
+  {id:8, name:'Pepaya', price:14000, img:'apel.jpg'},
+  {id:9, name:'Stroberi', price:39000, img:'apel.jpg'},
+  {id:10, name:'Alpukat', price:21500, img:'apel.jpg'},
+]
+    }));
 
-    updateCart() {
-      this.total = this.items.reduce((sum, item) => sum + item.total, 0);
-      this.quantity = this.items.reduce((sum, item) => sum + item.quantity, 0);
-    },
-  });
+    window.rupiah = function (number) {
+        return new Intl.NumberFormat('id-ID', {
+            style: 'currency',
+            currency: 'IDR',
+            minimumFractionDigits: 0
+        }).format(number);
+    }
 });
-
-// Format Rupiah
-function rupiah(number) {
-  return new Intl.NumberFormat("id-ID", {
-    style: "currency",
-    currency: "IDR",
-  }).format(number);
-}
