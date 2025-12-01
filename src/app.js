c82a26fd044705ab7098adc192c365b9c43ebe69
unknown <marinafebiyol@gmail.com>
2025-12-01 18:21:03 +0700

    membuat website payment gateaway

--- src/app.js
+++ src/app.js
@@ -40,9 +40,15 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('products', () => ({
         items: [
             {id:1, name:'Apel', price:20000, img:'apel.jpg'},
-            {id:2, name:'Anggur', price:15000, img:'anggur.jpg'},
-            {id:3, name:'Alpukat', price:17000, img:'alpukat.jpg'},
-            {id:4, name:'Pisang', price:18000, img:'pisang.jpg'}
+            {id:2, name:'Anggur', price:25000, img:'anggur.jpg'},
+            {id:3, name:'Jeruk', price:27000, img:'jeruk.jpg'},
+            {id:4, name:'Kiwi', price:32000, img:'kiwi.jpg'},
+            {id:5, name:'Semangka', price:17000, img:'semangka.jpg'},
+            {id:6, name:'Mangga', price:17000, img:'mangga.jpg'},
+            {id:7, name:'Pepaya', price:17000, img:'pepaya.jpg'},
+            {id:8, name:'Stroberi', price:17000, img:'stroberi.jpg'},
+            {id:9, name:'Alpukat', price:17000, img:'alpukat.jpg'},
+            {id:10, name:'Pisang', price:18000, img:'pisang.jpg'}
         ]
     }));
 
